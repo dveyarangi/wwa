1c8bd103ece6e13b468687750df9baec52c961fc
dveyarangi <dveyarangi@gmail.com>
2013-08-21 20:50:17 +0300

    setup

---
+++ eir-desktop/src/com/nnf/eir/Main.java
@@ -0,0 +1,16 @@
+package com.nnf.eir;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+
+public class Main {
+	public static void main(String[] args) {
+		LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
+		cfg.title = "eir";
+		cfg.useGL20 = false;
+		cfg.width = 480;
+		cfg.height = 320;
+		
+		new LwjglApplication(new EirMain(), cfg);
+	}
+}
