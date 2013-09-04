38eb99c6480748b6c6533f89e6917b1f0b3fd638
jajdoo <jajdoo@gmail.com>
2013-09-03 22:30:05 -0700

    basic mouse drag/zoom

--- eir/src/eir/game/screens/GameScreen.java
+++ eir/src/eir/game/screens/GameScreen.java
@@ -1,24 +1,19 @@
 package eir.game.screens;
 
-import java.util.Iterator;
-import java.util.LinkedList;
-import java.util.List;
-
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.InputMultiplexer;
 import com.badlogic.gdx.graphics.GL10;
 import com.badlogic.gdx.graphics.OrthographicCamera;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.Texture.TextureFilter;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.Body;
 import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
 import com.badlogic.gdx.physics.box2d.World;
+
 import eir.game.EirGame;
-import eir.resources.BodyLoader;
-import eir.resources.GameFactory;
+import eir.input.GameInputProcessor;
+import eir.input.UIInputProcessor;
 import eir.resources.Level;
 import eir.resources.LevelLoader;
 import eir.world.Asteroid;
@@ -32,10 +27,10 @@ import eir.world.Asteroid;
  */
 public class GameScreen extends AbstractScreen
 {
+	private InputMultiplexer inputMultiplexer;
+	
 	private OrthographicCamera camera;
 	private SpriteBatch batch;
-	private Texture texture;
-	private Sprite sprite;
 
 	private static final Vector2 GRAVITY = Vector2.Zero;
 
@@ -53,7 +48,7 @@ public class GameScreen extends AbstractScreen
 		float w = Gdx.graphics.getWidth();
 		float h = Gdx.graphics.getHeight();
 
-		camera = new OrthographicCamera( 1, h / w );
+		camera = new OrthographicCamera( w, h );
 		batch = new SpriteBatch();
 		
 		physicsWorld = new World( GRAVITY, true/* sleep */);
@@ -66,6 +61,10 @@ public class GameScreen extends AbstractScreen
 
 		level = loader.readLevel( physicsWorld, levelName );
 		level.init();
+		
+		inputMultiplexer = new InputMultiplexer();
+		inputMultiplexer.addProcessor( new UIInputProcessor() );
+		inputMultiplexer.addProcessor( new GameInputProcessor(camera) );
 	}
 
 	@Override
@@ -74,10 +73,14 @@ public class GameScreen extends AbstractScreen
 		super.render( delta );
 		Gdx.gl.glClearColor( 0, 0, 0, 1 );
 		Gdx.gl.glClear( GL10.GL_COLOR_BUFFER_BIT );
-
+		
 		Body body;
 		Sprite sprite;
+		
 		batch.begin();
+		batch.setProjectionMatrix( camera.projection );
+		batch.setTransformMatrix( camera.view );
+		
 		for(Asteroid asteroid : level.getAsteroids())
 		{
 			body = asteroid.getModel().getBody();
@@ -86,6 +89,7 @@ public class GameScreen extends AbstractScreen
 			sprite.setRotation( body.getAngle() );
 			sprite.draw( batch );
 		}
+		
 		batch.end();
 		
 		debugRenderer.render( physicsWorld, camera.combined );
@@ -102,6 +106,7 @@ public class GameScreen extends AbstractScreen
 	public void show()
 	{
 		super.show();
+		Gdx.input.setInputProcessor( inputMultiplexer );
 	}
 
 	@Override
---
+++ eir/src/eir/input/GameInputProcessor.java
@@ -0,0 +1,86 @@
+package eir.input;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.InputProcessor;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+
+/**
+ * handles input for game
+ * @author Ni
+ *
+ */
+public class GameInputProcessor implements InputProcessor
+{
+	private OrthographicCamera camera;
+	
+	private int lastx = -1;
+	private int lasty = -1;
+	
+	public GameInputProcessor(OrthographicCamera camera)
+	{
+		this.camera = camera;
+	}
+
+	@Override
+	public boolean keyDown(int keycode)
+	{
+		return false;
+	}
+
+	@Override
+	public boolean keyUp(int keycode)
+	{
+		return false;
+	}
+
+	@Override
+	public boolean keyTyped(char character)
+	{
+		return false;
+	}
+
+	@Override
+	public boolean touchDown(int screenX, int screenY, int pointer, int button)
+	{
+		if( button==Input.Buttons.LEFT )
+		{
+			lastx = screenX;
+			lasty = screenY;
+		}
+		return true;
+	}
+
+	@Override
+	public boolean touchUp(int screenX, int screenY, int pointer, int button)
+	{
+		return false;
+	}
+
+	@Override
+	public boolean touchDragged(int screenX, int screenY, int pointer)
+	{
+		camera.position.x -= (screenX-lastx)*camera.zoom;
+		camera.position.y += (screenY-lasty)*camera.zoom;
+		camera.update();
+		lastx = screenX;
+		lasty = screenY;
+		return true;
+	}
+
+	@Override
+	public boolean mouseMoved(int screenX, int screenY)
+	{
+		return false;
+	}
+
+	@Override
+	public boolean scrolled(int amount)
+	{
+		camera.zoom += amount*0.1;
+		camera.update();
+		
+		return true;
+	}
+
+}
