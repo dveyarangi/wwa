93fc881dbad7742855264fcd5755485a08bc6a27
jajdoo <jajdoo@gmail.com>
2013-09-05 19:17:27 -0700

    taters
    max zoom in thingy

--- eir/src/eir/input/CameraController.java
+++ eir/src/eir/input/CameraController.java
@@ -10,22 +10,20 @@ import com.badlogic.gdx.math.Vector3;
  */
 public class CameraController
 {
-	public float maxZoomOut;
-	public float maxZoomIn;
+	public float maxZoomOut = 0.1f;
+	public float maxZoomIn = 100;
 	public final OrthographicCamera camera;
 	
 	private Vector3 f = new Vector3(0,0,0); // force
 	private Vector3 v = new Vector3(0,0,0); // velocity
 	private Vector3 a = new Vector3(0,0,0); // acceleration
-	
-	private float b; // Viscosity coefficient
+	private float b = 15; // Viscosity coefficient
 	
 	public CameraController( OrthographicCamera camera )
 	{
 		this.camera = camera;
 		maxZoomIn = 0.1f;
 		maxZoomOut = 100;
-		b = 10;
 	}
 	
 	
@@ -42,8 +40,7 @@ public class CameraController
 	{
 		if( camera.zoom < maxZoomIn )
 		{
-			a.set(0, 0, 0);
-			v.set(0, 0, 0);
+			injectImpulse(0, 0, maxZoomIn);
 			camera.zoom = maxZoomIn;
 		}
 		else if (camera.zoom > maxZoomOut )
@@ -53,9 +50,6 @@ public class CameraController
 			camera.zoom = maxZoomOut;
 		}
 		
-		a.z = f.z - b*v.z;
-		v.z += a.z * delta;
-		
 		a.set( f.tmp().sub(v.tmp2().mul(b)) );
 		v.add( a.tmp().mul(delta) );
 		f.set(0, 0, 0);
@@ -63,17 +57,6 @@ public class CameraController
 		camera.zoom += v.z*delta + a.z*delta*delta/2;
 		camera.position.x += v.x*delta + a.x*delta*delta/2;
 		camera.position.y += v.y*delta + a.y*delta*delta/2;
-		
-//		zoomAcc = zoomF - b*zoomV;
-//		zoomV   = zoomV + zoomAcc*delta;
-//		zoomF = 0;
-		
-//		zoomAcc = Math.abs(zoomAcc)<0.0000001 ? 0 : zoomF - b*zoomV;
-//		zoomV   = Math.abs(zoomAcc)<0.0000001 ? 0 : zoomV + zoomAcc*delta;
-		
-		//camera.zoom += amount*0.1*camera.zoom;
-		//camera.position.x -= amount*camera.zoom*0.1*(lastx - camera.viewportWidth/2);
-		//camera.position.y += amount*camera.zoom*0.1*(lasty - camera.viewportHeight/2);
 		
 		camera.update();
 	}
