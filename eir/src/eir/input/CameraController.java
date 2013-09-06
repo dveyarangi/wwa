ff97acee5023db9c3cb85140ebc511b2cd9747e1
jajdoo <jajdoo@gmail.com>
2013-09-05 18:23:35 -0700

    dafuq?

--- eir/src/eir/input/CameraController.java
+++ eir/src/eir/input/CameraController.java
@@ -1,6 +1,7 @@
 package eir.input;
 
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.math.Vector3;
 
 /**
  * controls camera 
@@ -10,26 +11,33 @@ import com.badlogic.gdx.graphics.OrthographicCamera;
 public class CameraController
 {
 	public float maxZoomOut;
+	public float maxZoomIn;
 	public final OrthographicCamera camera;
 	
-	private float zoomF;
-	private float zoomV;
-	private float zoomAcc;
+	private Vector3 f = new Vector3(0,0,0); // force
+	private Vector3 v = new Vector3(0,0,0); // velocity
+	private Vector3 a = new Vector3(0,0,0); // acceleration
+	
 	private float b; // Viscosity coefficient
 	
 	public CameraController( OrthographicCamera camera )
 	{
 		this.camera = camera;
-		zoomF = 0;
-		zoomV = 0;
-		zoomAcc = 0;
+		maxZoomIn = 0.1f;
+		maxZoomOut = 100;
+//		zoomF = 0;
+//		zoomV = 0;
+//		zoomAcc = 0;
 		b = 10;
+		//b = 1/camera.zoom;
 	}
 	
 	
 	public void injectImpulse( float x, float y, float z )
 	{
-		zoomF += z*camera.zoom*100;
+//		zoomF += z*camera.zoom*100;
+		f.z = z*100*camera.zoom;
+		//f.add(x, y, z*100);
 	}
 	
 	/**
@@ -38,12 +46,30 @@ public class CameraController
 	 */
 	public void cameraStep( float delta )
 	{
-		System.out.println( "F: "+ zoomF + " acc: "+zoomAcc +" v: "+zoomV+" zoom: "+camera.zoom);
-		camera.zoom += zoomV*delta + zoomAcc*delta*delta/2;
-		zoomAcc = zoomF - b*zoomV;
-		zoomV   = zoomV + zoomAcc*delta;
-		b = camera.zoom;
-		zoomF = 0;
+		if( camera.zoom < maxZoomIn )
+		{
+			a.set(0, 0, 0);
+			v.set(0, 0, 0);
+			camera.zoom = maxZoomIn;
+		}
+		else if (camera.zoom > maxZoomOut )
+		{
+			a.set(0, 0, 0);
+			v.set(0, 0, 0);
+			camera.zoom = maxZoomOut;
+		}
+		
+		a.z = f.z - b*v.z;
+		v.z += a.z * delta;
+//		a.set( f.sub(v.mul(b)) );
+//		v.add( a.mul(delta) );
+		f.set(0, 0, 0);
+		camera.zoom += v.z*delta + a.z*delta*delta/2;
+		
+		
+//		zoomAcc = zoomF - b*zoomV;
+//		zoomV   = zoomV + zoomAcc*delta;
+//		zoomF = 0;
 		
 //		zoomAcc = Math.abs(zoomAcc)<0.0000001 ? 0 : zoomF - b*zoomV;
 //		zoomV   = Math.abs(zoomAcc)<0.0000001 ? 0 : zoomV + zoomAcc*delta;
