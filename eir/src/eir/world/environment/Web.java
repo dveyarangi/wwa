c6ebc2a470d7ed9ac0257c84202c7ae6403e5993
jajdoo <jajdoo@gmail.com>
2013-09-09 23:36:08 -0700

    them webs attach to all sorts of places!

--- eir/src/eir/world/environment/Web.java
+++ eir/src/eir/world/environment/Web.java
@@ -41,19 +41,27 @@ public class Web
 		targetSprite.draw(spriteBatch);
 	}
 	
-	public void init( Asteroid source, Asteroid target )
+	public void init( Asteroid sourceAst, Asteroid targetAst )
 	{
+		System.out.println(source.getX()+ " "+source.getY());
+		Vector2 src = Vector2.tmp.set( sourceAst.getX() + sourceAst.getModel().getSprite().getWidth()*(source.getX()-0.5f), 
+				 					   sourceAst.getY() + sourceAst.getModel().getSprite().getHeight()*(source.getY()-0.5f) );
+
+		Vector2 tar = Vector2.tmp2.set( targetAst.getX() + targetAst.getModel().getSprite().getWidth()*(target.getX()-0.5f), 
+				   					    targetAst.getY() + targetAst.getModel().getSprite().getHeight()*(target.getY()-0.5f) );
+
+		
 		sourceSprite = new Sprite( new TextureRegion(this.sourceTexture, 512, 256) );
 		targetSprite = new Sprite( new TextureRegion(this.targetTexture, 512, 256) );
 		threadSprite = new Sprite( new TextureRegion(this.threadTexture, 1024, 256) );
 		
-		sourceSprite.setPosition( source.getX() - sourceSprite.getWidth()/2, source.getY() - sourceSprite.getHeight()/2 );
-		targetSprite.setPosition( target.getX() - targetSprite.getWidth()/2, target.getY() - targetSprite.getHeight()/2 );		
-		threadSprite.setPosition( source.getX() + (target.getX() - source.getX())/2 - threadSprite.getWidth()/2,
-								  source.getY() + (target.getY() - source.getY())/2 - threadSprite.getHeight()/2);
+		sourceSprite.setPosition( src.x - sourceSprite.getWidth()/2, src.y - sourceSprite.getHeight()/2 );
+		targetSprite.setPosition( tar.x - targetSprite.getWidth()/2, tar.y - targetSprite.getHeight()/2 );		
+		threadSprite.setPosition( src.x + (tar.x - src.x)/2 - threadSprite.getWidth()/2,
+								  src.y + (tar.y - src.y)/2 - threadSprite.getHeight()/2);
 		
 		
-		Vector2 v = Vector2.tmp.set(target.getX() - source.getX(), target.getY() - source.getY());
+		Vector2 v = Vector2.tmp.set(tar.x - src.x, tar.y - src.y);
 		
 		float angle = v.angle();
 		targetSprite.rotate( angle );
